1a686f8be9ee39307813b33b53789984bc7fcc91
Remy Oudemans <remyoudemans@gmail.com>
2018-09-01 16:19:00 +0200

    made initial connection to local api experiment

--- src/features/welcome/type-here.tsx
+++ src/features/welcome/type-here.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { changeItemAtIndex, funcIf } from '../../utils'
 import { TextInput } from '../../components/text-input'
 import { Chapter } from './chapter'
-import { Button } from "../../components/button";
+import { Button } from '../../components/button'
 
 interface TypeHereState {
 	inputValue: string
@@ -13,93 +13,130 @@ interface TypeHereState {
 }
 
 export class TypeHere extends React.Component<{}, {}> {
-         public state: TypeHereState = { inputValue: "", titleCount: 0, titles: [], chapterContents: [], unsubmittedChapterContents: [] };
+	public state: TypeHereState = {
+		inputValue: '',
+		titleCount: 0,
+		titles: [],
+		chapterContents: [],
+		unsubmittedChapterContents: []
+	}
 
-         private titleInputRef = React.createRef<HTMLInputElement>();
+	public componentDidMount() {
+		fetch('http://localhost:3001/example-story', {
+			mode: 'cors'
+		})
+			.then(response => {
+				if (response.status !== 200) {
+					console.log(
+						'Looks like there was a problem. Status Code: ' + response.status
+					)
+					return
+				}
 
-         private focusOnTitleInput = () => {
-           const inputRef = this.titleInputRef.current as HTMLInputElement;
-           inputRef.focus();
-         };
+				// Examine the text in the response
+				response.json().then(data => {
+					console.log(data)
+				})
+			})
+			.catch(err => {
+				console.log('Fetch Error :-S', err)
+			})
+	}
 
-         private onType = (e: React.ChangeEvent<HTMLInputElement>) => {
-           this.setState({ inputValue: e.target.value });
-         };
+	private titleInputRef = React.createRef<HTMLInputElement>()
 
-         private submitTitle = () => {
-           this.setState((prevState: TypeHereState) => ({
-             inputValue: "",
-             titleCount: prevState.titleCount + 1,
-             titles: [...prevState.titles, prevState.inputValue],
-             chapterContents: [...prevState.chapterContents, ""],
-             unsubmittedChapterContents: [
-               ...prevState.unsubmittedChapterContents,
-               ""
-             ]
-           }));
-         };
+	private focusOnTitleInput = () => {
+		const inputRef = this.titleInputRef.current as HTMLInputElement
+		inputRef.focus()
+	}
 
-         private onTitleSubmit = () => {
-           funcIf(this.state.inputValue !== "", this.submitTitle);
-         };
+	private onType = (e: React.ChangeEvent<HTMLInputElement>) => {
+		this.setState({ inputValue: e.target.value })
+	}
 
-         private onChapterContentChange = (value: string, index: number) => {
-           this.setState((prevState: TypeHereState) => ({
-             unsubmittedChapterContents: changeItemAtIndex(
+	private submitTitle = () => {
+		this.setState((prevState: TypeHereState) => ({
+			inputValue: '',
+			titleCount: prevState.titleCount + 1,
+			titles: [...prevState.titles, prevState.inputValue],
+			chapterContents: [...prevState.chapterContents, ''],
+			unsubmittedChapterContents: [...prevState.unsubmittedChapterContents, '']
+		}))
+	}
 
+	private onTitleSubmit = () => {
+		funcIf(this.state.inputValue !== '', this.submitTitle)
+	}
 
+	private onChapterContentChange = (value: string, index: number) => {
+		this.setState((prevState: TypeHereState) => ({
+			unsubmittedChapterContents: changeItemAtIndex(
+				prevState.unsubmittedChapterContents,
+				index,
+				value
+			)
+		}))
+	}
 
+	private onChapterContentSubmit = (index: number) => {
+		this.setState(
+			(prevState: TypeHereState) => ({
+				chapterContents: changeItemAtIndex(
+					prevState.chapterContents,
+					index,
+					prevState.unsubmittedChapterContents[index]
+				),
+				unsubmittedChapterContents: changeItemAtIndex(
+					prevState.unsubmittedChapterContents,
+					index,
+					''
+				)
+			}),
+			this.focusOnTitleInput
+		)
+	}
 
+	private makeChapterContentEditable = (index: number) => {
+		this.setState((prevState: TypeHereState) => ({
+			chapterContents: changeItemAtIndex(prevState.chapterContents, index, ''),
+			unsubmittedChapterContents: changeItemAtIndex(
+				prevState.unsubmittedChapterContents,
+				index,
+				prevState.chapterContents[index]
+			)
+		}))
+	}
 
-
-               prevState.unsubmittedChapterContents,
-               index
-,
-               value
-             )
-           }));
-         };
-
-         private onChapterContentSubmit = (index: number) => {
-           this.setState((prevState: TypeHereState) => ({ chapterContents: changeItemAtIndex(prevState.chapterContents, index, prevState.unsubmittedChapterContents[index]), unsubmittedChapterContents: changeItemAtIndex(prevState.unsubmittedChapterContents, index, "") }), this.focusOnTitleInput);
-         };
-
-         private makeChapterContentEditable = (index: number) => {
-           this.setState((prevState: TypeHereState) => ({
-             chapterContents: changeItemAtIndex(
-               prevState.chapterContents,
-               index,
-               ""
-             ),
-             unsubmittedChapterContents: changeItemAtIndex(
-               prevState.unsubmittedChapterContents,
-               index,
-               prevState.chapterContents[index]
-             )
-           }));
-         };
-
-         public render() {
-           const { inputValue, titles, chapterContents, unsubmittedChapterContents } = this.state;
-           return <>
-               <TextInput innerRef={this.titleInputRef} placeholder={`title            chapter ${titles.length + 1}`} value={inputValue} onChange={this.onType} onEnter={this.onTitleSubmit} />
-               <Button onClick={this.onTitleSubmit} text="Click me!" />
-               {titles.map((title, index) => (
-                 <Chapter
-                   key={`submissions-chapter-${index}`}
-                   title={title}
-                   chapterIndex={index}
-                   chapterText={chapterContents[index]}
-                   unsubmittedChapterText={
-                     unsubmittedChapterContents[index]
-                   }
-                   onChapterTextChange={this.onChapterContentChange}
-                   onChapterTextSubmit={this.onChapterContentSubmit}
-                   makeChapterContentEditable={
-                     this.makeChapterContentEditable
-                   }
-                 />
-               ))}
-             </>;
-         }
-       }
+	public render() {
+		const {
+			inputValue,
+			titles,
+			chapterContents,
+			unsubmittedChapterContents
+		} = this.state
+		return (
+			<>
+				<TextInput
+					innerRef={this.titleInputRef}
+					placeholder={`title chapter ${titles.length + 1}`}
+					value={inputValue}
+					onChange={this.onType}
+					onEnter={this.onTitleSubmit}
+				/>
+				<Button onClick={this.onTitleSubmit} text="Click me!" />
+				{titles.map((title, index) => (
+					<Chapter
+						key={`submissions-chapter-${index}`}
+						title={title}
+						chapterIndex={index}
+						chapterText={chapterContents[index]}
+						unsubmittedChapterText={unsubmittedChapterContents[index]}
+						onChapterTextChange={this.onChapterContentChange}
+						onChapterTextSubmit={this.onChapterContentSubmit}
+						makeChapterContentEditable={this.makeChapterContentEditable}
+					/>
+				))}
+			</>
+		)
+	}
+}
