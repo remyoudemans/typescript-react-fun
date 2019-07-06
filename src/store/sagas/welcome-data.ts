42ed9373cf4f070304993223b4c9c168d8e50beb
Remy Oudemans <remy.oudemans@digib.com>
2019-07-06 16:17:30 +0200

    improves dockerfile

--- src/store/sagas/welcome-data.ts
+++ src/store/sagas/welcome-data.ts
@@ -3,10 +3,6 @@ import { Welcome } from '../modules'
 import { requestApi } from './request-api'
 
 export function* getExampleStories() {
-    // TODO: add env magic so these can be set in env variables
-
-    // this is the one that works when using docker-compose
-	// const exampleStories = yield requestApi('http://story-builder-back-end/example-story')
 	const exampleStories = yield requestApi('http://localhost:3001/example-story')
 
 	if (exampleStories && exampleStories.titles && exampleStories.titles.length) {
